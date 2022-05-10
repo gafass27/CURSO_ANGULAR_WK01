4875d4980e06b300ef086ab8505c1dcfd718c863
unknown <andres.garayf@fac.mil.co>
2022-05-09 21:59:06 -0500

    semana 3

--- src/app/components/destino-detalle/destino-detalle.component.ts
+++ src/app/components/destino-detalle/destino-detalle.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, InjectionToken, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/app.module';
+import { DestinoViaje } from '../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/models/destino-viaje.model';
+import { DestinosApiClient } from '../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/models/destinos-api-client.model';
+
+/*
+class DestinosApiClientViejo {
+  getById(id: String): DestinoViaje {
+    console.log('llamando por la clase vieja!');
+    return null;
+  }
+}
+
+interface AppConfig {
+  apiEndpoint: String;
+}
+
+const APP_CONFIG_VALUE: AppConfig = {
+  apiEndpoint: 'mi_api.com'
+};
+
+const APP_CONFIG = new InjectionToken<AppConfig>('app.config');
+
+class DestinosApiClientDecorated extends DestinosApiClient {
+  constructor(@Inject(APP_CONFIG) private config: AppConfig, store: Store<AppState>) {
+    super(store);
+  }
+  getById(id: String): DestinoViaje {
+    console.log('llamando por la clase decorada!');
+    console.log('config: ' + this.config.apiEndpoint);
+    return super.getById(id);
+  }
+}
+*/
 
 @Component({
   selector: 'app-destino-detalle',
-  templateUrl: './destino-detalle.component.html',
-  styleUrls: ['./destino-detalle.component.css']
+  templateUrl: '../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/components/destino-detalle/destino-detalle.component.html',
+  styleUrls: ['../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/components/destino-detalle/destino-detalle.component.css'],
+  providers: [DestinosApiClient //,
+    //{ provide: APP_CONFIG, useValue: APP_CONFIG_VALUE },
+    //{ provide: DestinosApiClient, useClass: DestinosApiClientDecorated },
+    //{ provide: DestinosApiClientViejo, useExisting: DestinosApiClient }
+
+  ]
 })
 export class DestinoDetalleComponent implements OnInit {
+  destino: DestinoViaje;
 
-  constructor() { }
+  //constructor(private route: ActivatedRoute, private destinosApiClient: DestinosApiClientViejo) { }
+
+  constructor(private route: ActivatedRoute, private destinosApiClient: DestinosApiClient) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.destino = this.destinosApiClient.getById(id);
   }
 
 }
--- src/app/components/lista-destinos/lista-destinos.component.ts
+++ src/app/components/lista-destinos/lista-destinos.component.ts
@@ -1,52 +1,81 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {DestinoViajeComponent} from "../destino-viaje/destino-viaje.component";
-import {DestinoViajeModel} from "../../models/destino-viaje-model";
-import {DestinosApiClientModel} from "../../models/destinos-api-client.model";
-import {Store} from "@ngrx/store";
-import {AppState} from "../../app.module";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/app.module';
+import { ElegidoFavoritoAction, NuevoDestinoAction } from '../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/models/destinos-viajes-state.model';
+import { DestinoViaje } from '../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/models/destino-viaje.model';
+//import { DestinosApiClient } from './../../models/destinos-api-client.model';
 
 @Component({
   selector: 'app-lista-destinos',
-  templateUrl: './lista-destinos.component.html',
-  styleUrls: ['./lista-destinos.component.css']
+  templateUrl: '../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/components/lista-destinos/lista-destinos.component.html',
+  styleUrls: ['../../../../../Nueva carpeta/abc/cursoAustralAngular/angular-wishlist/src/app/components/lista-destinos/lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
-
-  destinos: DestinoViajeModel[];
-
-  @Output() onItemAdded: EventEmitter<DestinoViajeModel>
+  @Output() onItemAdded: EventEmitter<DestinoViaje>;
   updates: string[];
+  all;
 
-  constructor(private destinosApiClient:DestinosApiClientModel,private store: Store<AppState>) {
+	/*destinos: string[];*/
+  //destinos: DestinoViaje[];
+  //destinosApiClient: DestinosApiClient;
+  constructor(private destinosApiClient: DestinosApiClient, private store: Store<AppState>) {
+	/*this.destinos = ['Atenas','Machu Pichu','Kyoto','Los Cabos']; */
+    //this.destinos = [];
     this.onItemAdded = new EventEmitter();
-    this.destinos = [];
     this.updates = [];
     this.store.select(state => state.destinos.favorito)
-      .subscribe(data=>{
-          if( data != null){
-            this.updates.push('se ha elegido ' + d.nombre);
-          }
-        })
-    /*this.destinosApiClient.subcribeOChange((d:DestinoViajeModel)=>{
-      if(d != null){
-        this.updates.push('se ha elegido ' + d.nombre);
+      .subscribe(data => {
+        //const fav = data;
+        if (data != null) {
+          this.updates.push('Se ha elegido a ' + data.nombre);
+        }
+      });
+    /*this.destinosApiClient.subscribeOnChange((d: DestinoViaje) => {
+      if (d != null) {
+        this.updates.push('Se ha elegido a ' + d.nombre);
       }
     });*/
+    this.all = null;
+    store.select(state => state.destinos.items).
+      subscribe(items => this.all = items);
   }
 
-  ngOnInit(){
+  ngOnInit(): void {
   }
 
-  elegido(element: DestinoViajeModel  ){
-    this.destinosApiClient.elegir(element);
-    this.store.dispatch(new ElegidoFavoritoAction(e))
 
-  }
+  /* Mejor usaremos agregar ahora que extirpamos la captura
+   * guardar(nombre:string, url:string):boolean {
+    console.log(nombre);
+    console.log(url);
+    console.log(new DestinoViaje(nombre, url));
+    this.destinos.push(new DestinoViaje(nombre, url));
+    console.log(this.destinos);
+    return false;
+  } */
 
-  agregado(d: DestinoViajeModel) {
+  agregar(d: DestinoViaje) {
     this.destinosApiClient.add(d);
-    this.onItemAdded.emit(d)
-    this.store.dispatch(new NuevoFavoritoAction(e))
+    this.onItemAdded.emit(d);
+    //this.store.dispatch(new NuevoDestinoAction(d));
+    //return false;
+
+  }
+
+  elegido(d: DestinoViaje) {
+    /*this.destinosApiClient.getAll().forEach(
+      function (x) {
+        x.setSelected(false);
+      }
+    );
+    d.setSelected(true);
+    */
+    this.destinosApiClient.elegir(d);
+    //this.store.dispatch(new ElegidoFavoritoAction(d));
+  }
+
+  getAll() {
+
   }
 
 }
